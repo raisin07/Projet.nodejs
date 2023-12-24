9cb19cd2a0ba924abd86f46706292fd11e4bc0fc
maureeend <drouard.maureen2@gmail.com>
2023-12-24 16:12:12 +0100

    ajt controllers et modification de server

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
-
 const express = require("express");
+const checkRequestFormat = require("./controllers/checkRequestFormat");
+const errorHandler = require("./controllers/errorHandler");
+const orderRoutes = require('./routes/orders.js');
+const productRoutes = require('./routes/products.js');
+const userRoutes = require('./routes/users.js');
+const securityRouter = require("./routes/security");
+
+// Connexion de la base de donnée
+require("./models/config");
 
 const app = express();
-const PORT = 3000;
 
-// On configure les routes ici
+// On configure les routes dans le dossier routes 
 
-app.get("/post", (req,res) => {
-    res.json({ message : "Voici les données "});
-});
-    
+// Celui ci était un test
+// app.use("/post", require("./routes/post.js"));
+
+app.use(checkRequestFormat);
+app.use(express.json());
+
+app.use(securityRouter);
+
+// Orders
+app.use('/orders', orderRoutes);
+
+// Users
+app.use('/users', userRoutes);
+
+//Products
+app.use('/products', productRoutes);
+
+app.use(errorHandler);
 
 // Lancer le serveur
+const PORT = 5000;
 app.listen(PORT, () => console.log('Le server est sur le port ' + PORT));
