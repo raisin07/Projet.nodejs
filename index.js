86b2dfb1abb6d4580a5d57c5ade713cd1f45c9a3
raisin07 <151188105+raisin07@users.noreply.github.com>
2023-12-24 12:26:54 +0100

    Add files via upload
    ajout de user.js et index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-true
-false
+const connection = require("./config.js");
+const User = require("./user.js");
+const Order = require("./order.js");
+const Product = require("./product.js");
+
+// Associations entre model
+
+module.exports = {
+  User,
+  Order,
+  Product,
+  connection,
+};
