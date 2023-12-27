bb1d8ea880fdafcd49e23ee17e049b257b1928aa
raisin07 <reizenebh@gmail.com>
2023-12-27 14:48:26 +0100

    suppression commentaires

--- models/index.js
+++ models/index.js
@@ -6,17 +6,17 @@ const Avis = require("./avis.js");
 const connection = require('./config');
 
 // Associations
-Product.hasMany(Order); // Un produit peut être dans plusieurs commandes
-Order.belongsTo(Product); // Une commande a un produit spécifique
+Product.hasMany(Order); 
+Order.belongsTo(Product); 
 
-User.hasMany(Order); // Un utilisateur peut avoir plusieurs commandes
-Order.belongsTo(User); // Une commande appartient à un utilisateur
+User.hasMany(Order); 
+Order.belongsTo(User); 
 
-User.hasMany(Avis); // Un utilisateur peut avoir plusieurs avis
-Avis.belongsTo(User); // Un avis appartient à un utilisateur
+User.hasMany(Avis); 
+Avis.belongsTo(User); 
 
-Product.hasMany(Avis); // Un produit peut avoir plusieurs avis
-Avis.belongsTo(Product); // Un avis est pour un produit spécifique
+Product.hasMany(Avis); 
+Avis.belongsTo(Product); 
 
 module.exports = {
   connection,
